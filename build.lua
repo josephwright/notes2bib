8aa4cd8464998a3ffca4cdb76aecb2be0af23b0f
Joseph Wright <joseph.wright@morningstar2.co.uk>
2021-11-22 21:55:51 +0000

    Update build.lua

--- build.lua
+++ build.lua
@@ -1,9 +1,6 @@
-#!/usr/bin/env texlua
-
 -- Build script for "notes2bib" files
 
--- Identify the bundle and module
-bundle = ""
+-- Identify the module
 module = "notes2bib"
 
 -- .dtx is self-contained
@@ -14,10 +11,4 @@ packtdszip  = true
 
 -- No implementation as standard
 typesetcmds = "\\AtBeginDocument{\\DisableImplementation}"
-
--- Find and run the build system
-kpse.set_program_name("kpsewhich")
-if not release_date then
-  dofile(kpse.lookup("l3build.lua"))
-end
 
